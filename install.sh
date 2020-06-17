f11c9822aa2e949c08346c31a567027119bdc534
catx0rr <catx0rr@gmail.com>
2020-06-17 14:22:33 +0800

    updated cleaned

--- install.sh
+++ install.sh
@@ -4,53 +4,101 @@
 
 # Initiate files
 
-echo "Downloading dependencies.."
-sudo echo
-sudo apt install -y cmake gcc
-echo 
-echo "Removing current configuration files.."
-echo
-sleep 1
-rm -rf $HOME/.vim
-rm -rf $HOME/.tmux
-rm -rfv $HOME/.vimrc
-rm -rfv $HOME/.tmux.conf
-rm -rfv $HOME/.bash_logout
-echo 
+banner='      .__                                      
+___  _|__| _____  __ _____  __________   ____  
+\  \/ /  |/     \|  |  \  \/  /\_  __ \_/ ___\ 
+ \   /|  |  Y Y  \  |  />    <  |  | \/\  \___ 
+  \_/ |__|__|_|  /____//__/\_ \ |__|    \___  >
+               \/            \/             \/ '
 
-echo "Copying resources.."
-echo
-sleep 2
-mkdir $HOME/.vim
-cp -frv conf/bashrc $HOME/.bashrc
-cp -frv conf/bash_logout $HOME/.bash_logout
-cp -frv conf/tmux.conf $HOME/.tmux.conf
-cp -frv conf/vimrc $HOME/.vimrc
-echo
+info='Done..
 
-echo "Installing plugins.."
-sleep 2
+NOTE: >>>
+
+If plugins does not work correctly, enable it manually:
+
+ctrl+b + I >> for tmux logging plugin (reload environment)
+open vim + :PlugInstall then :source % >> VIM plugins
+python3 .vim/plugged/YouCompleteMe/install.py --all
+ 
+Resource Links / Documentation:
+
+https://github.com/tmux-plugins/tmux-logging
+https://github.com/junegunn/vim-plug
+https://github.com/ycm-core/YouCompleteMe#linux-64-bit
+                                                      <<<'
+install() {
+
+# Driver function
+
+
+    echo
+    echo "Downloading dependencies.."
+    echo
+    sudo apt install -y cmake gcc
+    echo
+    echo 
+    echo "Removing current configuration files.."
+    echo
+    sleep 1
+    rm -rf $HOME/.vim
+    rm -rf $HOME/.tmux
+    rm -rfv $HOME/.vimrc
+    rm -rfv $HOME/.tmux.conf
+    rm -rfv $HOME/.bash_logout
+    echo 
+
+    echo "Copying resources.."
+    echo
+    sleep 2
+    mkdir $HOME/.vim
+    cp -frv conf/bashrc $HOME/.bashrc
+    cp -frv conf/bash_logout $HOME/.bash_logout
+    cp -frv conf/tmux.conf $HOME/.tmux.conf
+    cp -frv conf/vimrc $HOME/.vimrc
+    echo
+
+    echo "Installing plugins.."
+    sleep 2
 
 # tmux plugin
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-tmux source ~/.tmux.conf
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    tmux source ~/.tmux.conf
 
 # vim plugin
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+            https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo "Done.."
+    echo -e "$info"
+
+}
+
+
+# Start
+
+echo -e "$banner"
 echo
+echo "This is overwrite all your configuration files"
+echo ".vimrc"
+echo ".tmux.conf"
+echo ".bashrc"
+echo ".bash_logout"
 echo
-echo "If plugins doesn't work correctly, enable it manually:"
-echo
-echo "ctrl+b + I >> for tmux logging plugin (reload environment)"
-echo "open vim + :PlugInstall then :source % >> VIM plugins"
-echo "python3 .vim/plugged/YouCompleteMe/install.py --all"
-echo 
-echo "Resource Links / Documentation:"
-echo
-echo "https://github.com/tmux-plugins/tmux-logging"
-echo "https://github.com/junegunn/vim-plug"
-echo "https://github.com/ycm-core/YouCompleteMe#linux-64-bit"
-echo
+
+while true; do
+    read -p "Do you want to continue?(y/n): " yn
+    case $yn in
+        [yY]|[yY][eE][sS]) 
+            echo $yn
+            install
+            break
+            ;;
+        [nN]|[nN][oO])
+            echo $yn
+            exit
+            ;;
+        *) 
+            continue
+            ;;
+    esac
+done
